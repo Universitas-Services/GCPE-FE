61d68244ab6f2b42cb901e57d8c84e03bdb96ce5
Yovani Navas <yovaninavas10@gmail.com>
2026-03-10 16:00:50 -0400

    Componente calendario

--- src/components/shared/SharedDatePicker.tsx
+++ src/components/shared/SharedDatePicker.tsx
@@ -8,14 +8,24 @@ interface SharedDatePickerProps extends InputHTMLAttributes<HTMLInputElement> {
 export const SharedDatePicker = forwardRef<
   HTMLInputElement,
   SharedDatePickerProps
->(({ error, className = '', ...props }, ref) => {
+>(({ error, className = '', onClick, ...props }, ref) => {
   return (
     <input
       type="date"
       ref={ref}
-      className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+      className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer ${
         error ? 'border-red-500' : 'border-gray-300'
       } ${className}`}
+      onClick={(e) => {
+        try {
+          if ('showPicker' in HTMLInputElement.prototype) {
+            e.currentTarget.showPicker();
+          }
+        } catch (err) {
+          console.debug('Failed to show native date picker:', err);
+        }
+        onClick?.(e);
+      }}
       {...props}
     />
   );
